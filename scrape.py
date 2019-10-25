69cd5e419382193080ef43b867ec15645a81d9b8
SeanSCao <ssc543@nyu.edu>
2019-10-25 18:11:17 -0400

    updated comments

--- scrape.py
+++ scrape.py
@@ -40,15 +40,20 @@ def scrape_page(url):
         response = requests_session.get(url, timeout=10)
         soup = BeautifulSoup(response.text, 'lxml')
 
+        # testing limitations to number requests that can be made
         if 'blocked' in response.text:
             print('we have been blocked')
+        # if response of ok
         if response.status_code == 200:
+            # for every filing on page
             for filing in soup.find(attrs={'xmlns:autn': 'http://schemas.autonomy.com/aci/'}).findAll('tr', attrs={'class': None}):
+                        # if filing is an information table (what we are looking for)
                         if 'INFORMATION TABLE' in filing.find(attrs={'class': 'filing'}).text:
                             date = filing.find(attrs={'class': 'blue'}).text
                             text = filing.find(attrs={'class': 'filing'}).text
                             name = text[text.index('for')+4:]
                             href = filing.find(attrs={'class': 'filing'})['href']
+                            # fixing special cases where xml files were named incorrectly
                             if 'xml' in href:
                                 link = href[href.index('http'):href.index(".xml'")+4]
                                 # print(link)
@@ -56,18 +61,23 @@ def scrape_page(url):
                                 continue
 
                             try:
-                                # print(link)
+                                # get information table for scraping
                                 info_table_response = requests_session.get(link, timeout=11)
                                 time.sleep(5)
                                 info_table = BeautifulSoup(
                                     info_table_response.text, 'lxml-xml')
+                                
+                                # test for blocked by server status
                                 if 'blocked' in info_table_response.text:
                                     print('we have been blocked')
                                 # print(info_table_response.status_code)
+
+                                # if response of ok
                                 if info_table_response.status_code == 200:
                                     total = 0
                                     companies = []
                                     try:
+                                        # get data for company
                                         for row in info_table.find(attrs={'summary': 'Form 13F-NT Header Information'}).findAll('tr'):
                                             if row.find(attrs={'class': 'FormData'}):
                                                 company =   row.find(attrs={'class': 'FormData'}).text
@@ -87,6 +97,7 @@ def scrape_page(url):
                                             output.append(companies[i][1])
                                         data.append(output)
                                     except:
+                                        # if error finding information table
                                         data.append([name, date, total, 'Information Table Not Found'])
                             except requests.exceptions.ConnectionError as e:
                                 data.append([name, date, '', 'Information Table Not Found: Connection error, make sure you are connected to the internet'])
@@ -113,6 +124,7 @@ def scrape_page(url):
         print(e)    
     return data
 
+# generate all the urls to scrape based on how many pages of results there are
 def generate_urls(form_type):
     params['formType'] = form_type
     FHR_response = requests_session.get(
@@ -127,18 +139,22 @@ def generate_urls(form_type):
             for i in range(1,total_results,10):
                 all_urls.append('https://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=INFORMATION+TABLE&sort=Date&startDoc='+str(i)+'&numResults=10&isAdv=true&formType='+form_type+'&fromDate='+month+'/'+start_day+'/'+year+'&toDate='+month+'/'+end_day+'/'+year+'&stemming=true')
 
+# max the number of attempts made at 5
 requests_session = requests.Session()
 requests_session.mount('www.sec.gov', HTTPAdapter(max_retries=5))
 requests_session.mount('http://', HTTPAdapter(max_retries=5))
 requests_session.mount('https://', HTTPAdapter(max_retries=5))
 
+# find all urls to scrape for each form type
 generate_urls('Form13FHR')
 generate_urls('Form13FHRAD')
 
+# multiprocessing threads = 5
 if __name__ == '__main__':
     with multiprocessing.Pool(5) as p:
         records = p.map(scrape_page, all_urls)
 
+# write out data to funds.csv
 with open('funds.csv', 'w+', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     headers = ['Name', 'Filing Date', 'Total Assets', '1 Name', '2 Name', '3 Name', '4 Name', '5 Name', '6 Name', '7 Name', '8 Name',
